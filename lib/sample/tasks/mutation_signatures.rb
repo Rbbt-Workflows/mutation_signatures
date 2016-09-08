ca7c1ddc9ea2de6206ff00a5ef2bdd5c2d09e447
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-09-08 19:00:30 +0200

    nil

--- lib/sample/tasks/mutation_signatures.rb
+++ lib/sample/tasks/mutation_signatures.rb
@@ -1,8 +1,12 @@
-module Sample
+if Module === Sample and Workflow === Sample
+  module Sample
 
-  dep :genomic_mutations
-  dep MutationSignatures, :context_change_count, :mutations => :genomic_mutations, :organism => :organism, :watson => :watson
-  task :mutation_signature => :tsv do
-    TSV.get_stream step(:context_change_count)
+    dep :genomic_mutations
+    dep :organism
+    dep :watson
+    dep MutationSignatures, :context_change_count, :mutations => :genomic_mutations, :organism => :organism, :watson => :watson
+    task :mutation_signature => :tsv do
+      TSV.get_stream step(:context_change_count)
+    end
   end
 end
--- lib/study/tasks/mutation_signatures.rb
+++ lib/study/tasks/mutation_signatures.rb
@@ -1,45 +1,47 @@
 require 'sample/tasks/mutation_signatures' 
 
-module Study
+if Module === Study and Workflow === Study
+  module Study
 
-  dep Sample, :mutation_signature do |jobname,options|
-    jobs = Study.samples(jobname).collect do |sample|
-      next unless sample.has_genotype?
-      sample = [jobname, sample] * ":"
-      Sample.job(:mutation_signature, sample)
-    end.compact
-    Misc.bootstrap jobs do |job|
-      job.produce
-    end
-    jobs
-  end
-  task :cohort_signatures => :tsv do
-
-    samples = dependencies.collect{|dep| dep.clean_name }
-    fields = samples
-    signatures = TSV.setup({}, :key_field => "Change", :fields => fields, :type => :list, :cast => :to_i)
-    Step.wait_for_jobs dependencies
-    dependencies.each do |dep|
-      sample = dep.clean_name
-      signature = dep.load
-      sample_pos = samples.index sample
-      signature.each do |change,count|
-        signatures[change] ||= [0] * samples.length
-        signatures[change][sample_pos] = count
+    dep Sample, :mutation_signature do |jobname,options|
+      jobs = Study.samples(jobname).collect do |sample|
+        next unless sample.has_genotype?
+        sample = [jobname, sample] * ":"
+        Sample.job(:mutation_signature, sample)
+      end.compact
+      Misc.bootstrap jobs do |job|
+        job.produce
       end
+      jobs
+    end
+    task :cohort_signatures => :tsv do
+
+      samples = dependencies.collect{|dep| dep.clean_name }
+      fields = samples
+      signatures = TSV.setup({}, :key_field => "Change", :fields => fields, :type => :list, :cast => :to_i)
+      Step.wait_for_jobs dependencies
+      dependencies.each do |dep|
+        sample = dep.clean_name
+        signature = dep.load
+        sample_pos = samples.index sample
+        signature.each do |change,count|
+          signatures[change] ||= [0] * samples.length
+          signatures[change][sample_pos] = count
+        end
+      end
+
+      signatures
     end
 
-    signatures
+    tasks[:nmf_features] = MutationSignatures.tasks[:nmf_features]
+    tasks[:nmf_profile_plots] = MutationSignatures.tasks[:nmf_profile_plots]
+    task_dependencies[:nmf_features] = MutationSignatures.task_dependencies[:nmf_features]
+    task_dependencies[:nmf_profile_plots] = MutationSignatures.task_dependencies[:nmf_profile_plots]
+
+
+    tasks[:em_features] = MutationSignatures.tasks[:em_features]
+    tasks[:em_profile_plots] = MutationSignatures.tasks[:em_profile_plots]
+    task_dependencies[:em_features] = MutationSignatures.task_dependencies[:em_features]
+    task_dependencies[:em_profile_plots] = MutationSignatures.task_dependencies[:em_profile_plots]
   end
-
-  tasks[:nmf_features] = MutationSignatures.tasks[:nmf_features]
-  tasks[:nmf_profile_plots] = MutationSignatures.tasks[:nmf_profile_plots]
-  task_dependencies[:nmf_features] = MutationSignatures.task_dependencies[:nmf_features]
-  task_dependencies[:nmf_profile_plots] = MutationSignatures.task_dependencies[:nmf_profile_plots]
-
-
-  tasks[:em_features] = MutationSignatures.tasks[:em_features]
-  tasks[:em_profile_plots] = MutationSignatures.tasks[:em_profile_plots]
-  task_dependencies[:em_features] = MutationSignatures.task_dependencies[:em_features]
-  task_dependencies[:em_profile_plots] = MutationSignatures.task_dependencies[:em_profile_plots]
 end
